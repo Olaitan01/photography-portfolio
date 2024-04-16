4f98a156fdc4371c1e5c695957ed09491f62f7cf
unknown <abiodunfatunbi4@gmail.com>
2024-04-16 12:02:29 +0100

    implemeted about page

--- src/components/pages/about/about.jsx
+++ src/components/pages/about/about.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import HeroImg from "/src/assets/about/Image (17).png" ;
+import mobileHeroImg from "/src/assets/about/Image (18).png";
+
 function About(){
+    const [isMobile, setIsMobile] = useState();
+
+    useEffect(()=>{
+        function handleResize(){
+            setIsMobile(window.innerWidth < 746)
+        }
+        
+        window.addEventListener('resize',handleResize)
+
+        return()=>window.removeEventListener('resize',handleResize)
+
+
+    },[])
+
+
     return(
-        <>
-        <h1 className="text-[#e7aeae]" >Hello</h1>
+        < >
+        <div className="w-[90%] flex justify-center m-auto items-center">
+           <div>
+           <div>
+                {!isMobile ?  <img src={HeroImg} alt="Damien standing upright with his arms folded" className="w-full" /> :  <img src={mobileHeroImg} alt="Damien standing upright with his arms folded" className="w-[500px]" />}
+               
+            </div>
+            <div>
+
+            </div>
+           </div>
+        </div>
         </>
     )
 }
